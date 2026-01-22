82924656f8af0b9c58010b3319ff73391c829338
markocampos <markocampos.mail@gmail.com>
2026-01-22 22:42:04 +0800

    v1.3.3 (beta)

--- kegama_residences/settings.py
+++ kegama_residences/settings.py
@@ -60,7 +60,10 @@ if os.environ.get('DB_NAME'):
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
-            'PORT': os.environ.get('DB_PORT'),
+            'PORT': os.environ.get('DB_PORT', '3306'),
+            'OPTIONS': {
+                'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            },
         }
     }
 else:
@@ -68,9 +71,6 @@ else:
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
-        },
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         }
     }
 
